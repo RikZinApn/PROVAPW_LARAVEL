eeedad568f4184a8dcdd07c9768775754425f79c
RikZinApn <henriue.araujo45@etec.sp.gov.br>
2022-10-03 04:34:12 -0300

    ProvaLravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+        <h1>Prova de Pw</h1>
+        <br><br>
+        <a href="/cadastro"><button>Cadastrar Usuário</button></a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="/listar"><button>Listar Cadastros</button></a>  
+</body>
+</html>
